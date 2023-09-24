928e189e5928861e7ffc96c1a871aab6d0683aa6
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-09-24 18:50:37 -0300

    1

--- src/usuario/Cliente.java
+++ src/usuario/Cliente.java
@@ -5,16 +5,17 @@ import funcionalidadesInternas.ContaCorrente;
 import funcionalidadesInternas.ContaPoupança;
 
 public class Cliente {
-	
+
 	public static void main(String[] args) {
 		Conta cc = new ContaCorrente();
 		Conta cp = new ContaPoupança();
-		
+
 		cc.depositar(1000);
 		cc.transferir(cp, 200);
-		
+
 		cc.imprimirExtrato();
 		cp.imprimirExtrato();
+		cp.imprimirExtrato();
 	}
-	
+
 }
